9425091e095598a8f5438e12fe90675844306de3
Svitlana <Svitlana@DESKTOP-LP32KO5>
2022-12-19 04:58:34 +0200

    Добавьте файлы проекта.

---
+++ Lab_points_2.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x, y;
+    cout << "Enter coordinates x: ";
+    cin >> x;
+    cout << "Enter coordinates y: ";
+    cin >> y;
+
+    if (x > 0 && x < 2 && y>0 && y < 1)
+    {
+        cout << "Point is in the area" << endl;
+    }
+    else
+    {
+        cout << "Point is not in the area" << endl;
+    }
+
+    return 0;
+}
+
